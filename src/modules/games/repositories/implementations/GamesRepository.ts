996be38d819ead15615d0a0c149da8f36b10d651
Anderson Cavalcante <andersonfelipevieira@gmail.com>
2022-09-20 15:56:22 -0300

    bug fix

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,7 +14,7 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder().where("title ILIKE :title",  {title: `%${param}`}).getMany();
+      .createQueryBuilder().where("title ILIKE :title",  {title: `%${param}%`}).getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
